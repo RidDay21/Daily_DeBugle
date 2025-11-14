7b0036496e7fbaada147f8669e613fd2d3b118b2
Fedor <f.brykin@g.nsu.ru>
2025-11-15 01:38:51 +0700

    UC-03

---
+++ DailyDeBugle/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using DailyDeBugle.Data;
+using DailyDeBugle.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyDeBugle.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _context.Users.OrderBy(u => u.Username).ToListAsync();
+        }
+    }
+}
