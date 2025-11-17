ec8b708a938d3b8c0fe3acaae3ad07355be2fdf7
RidDay21 <niko_laptev21@mail.ru>
2025-11-17 22:43:20 +0700

    Create Native navigation for first UC

--- DailyDeBugle/Models/Publicaion.cs
+++ DailyDeBugle/Models/Publicaion.cs
@@ -1,10 +1,24 @@
 namespace DailyDeBugle.Models
 {
+    /// <summary>
+    /// Класс енум для строгой типизации нашей периодичности
+    /// </summary>
+    public enum PublicationFrequency
+    {
+        Daily,
+        Weekly,
+        Monthly
+    }
+
     public class Publication
     {
         public int PublicationId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        /// <summary>
+        ///Сделал типизацию строгую
+        /// </summary>
+        public PublicationFrequency Frequency { get; set; } 
         public bool IsActive { get; set; } = true;
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         
